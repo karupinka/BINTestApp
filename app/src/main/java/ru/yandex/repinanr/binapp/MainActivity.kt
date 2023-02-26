bde5f38ca10bb06faba7140c2e05e530f0792c3e
Nadezhda Repina <repinanr@yandex.ru>
2023-02-26 17:15:10 +0300

    create project

---
+++ app/src/main/java/ru/yandex/repinanr/binapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.yandex.repinanr.binapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
